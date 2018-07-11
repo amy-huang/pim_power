17a798cce900e3e2fb50af22cba71b90d66f1ce2
amy-huang <amy.jiamei.huang@gmail.com>
2018-07-11 14:50:17 -0400

    mass power calcs

--- get_stats_and_config.bash
+++ get_stats_and_config.bash
@@ -3,34 +3,21 @@
 # This script copies the stats file from the result directory in SMC-WORK created by simulation,
 # and then sums important stats across all CPUs/Mem controllers that are added to a new stats file
 
-echo "Preparing config and stats."
-#################################################################################################################
-# Copying config and stats file over 
-
-config_path="$(find ../SMC-WORK/scenarios/$1/ -name config.json)"
-#original_stats_path="$(find ../SMC-WORK/scenarios/$1/ -name stats.txt)"
-original_stats_path="$(find ../SMC-WORK/scenarios/$1/ -name stats_gem5.txt)"
 newstats=./cut_stats.txt   #Make a copy of stats file in current directory to add sum stats to
 
-echo "	Copying over config.json file from simulation result directory to here..."
-#cp $config_path .
-
-echo "	Copying over stats.txt file from simulation result directory to here..."
-#cp $original_stats_path $newstats
-
-#################################################################################################################
 # Functions to create stats over CPUs, memory controllers, and etc.
 
 sum () {  # Pulls all stats containing first argument and sum; print as non-scientific notated integer
-    #sum=$(grep -w $1 $newstats | grep -v "pim_sys" | awk '{s+=$2}END{printf ("%1.0d",s)}')  #ignore pim CPU value
-    sum=$(grep -w $1 $newstats | awk '{s+=$2}END{printf ("%1.0d",s)}')  #include PIM core values
+    sum_cpu=$(grep $1 $newstats | grep -v "pim_sys" | awk '{s+=$2}END{printf ("%.1d",s)}')  #ignore pim CPU value
+    sum_pim=$(grep $1 $newstats | grep "pim_sys" | awk '{s+=$2}END{printf ("%1.1d",s)}')  #include PIM core values
    
     # Write to new stats file under name of second argument      
-    echo $2"                       "$sum >> $newstats
+    echo $2"                       "$sum_cpu >> $newstats
+    echo $3"                       "$sum_pim >> $newstats
 }
 
 sum_float () { # Same as sum except supports floats
-    sum=$(grep -w $1 $newstats | grep -v "pim_sys" | awk '{s+=$2}END{print s}')  #ignore pim CPU value
+    sum=$(grep $1 $newstats | grep -v "pim_sys" | awk '{s+=$2}END{print s}')  #ignore pim CPU value
     echo $2"                       "$sum >> $newstats
 }
 
@@ -47,47 +34,44 @@ peak () { # Find the highest value for some stat
 #################################################################################################################
 # Aggregating stats 
 
-sum system.mem_ctrls[0-9].totalEnergy   system.mem_ctrls.totalEnergy
-# First get total time simulated
-sum_float  sim_seconds                      total_sim_seconds 
-# Remove timestamp from sim_seconds entries, so they aren't deleted
-#sed -i -e 's/timestamp[0-9].sim_seconds/sim_seconds/g' $newstats
-# Remove all other lines without timestamp6 in front of them
-sed -i '/timestamp[1]/d' $newstats
-# Remove 'timestamp6.' from remaining stats
-#sed -i -e 's/timestamp4.//g' $newstats
-sed -i -e 's/timestamp[0-9].//g' $newstats
+# Get this first to subtract from total energy
+prep_energy=$(grep "totalEnergy" cut_stats.txt | grep "timestamp$2" | awk '{s+=$2}END{print s}')
+echo "system.prep.Energy                       "$prep_energy >> $newstats
 
+# Remove all other lines not in the execution time duration
+sed -i '/timestamp['$1']/d' $newstats
+# Remove timestamp from remaining stats
+sed -i -e 's/timestamp[0-9].//g' $newstats
+# Sum the stats that aren't cumulative, and are only for the execution time
 echo "	Aggregating stats across all CPUs, memory controllers."
-sum  system.cpu[0-9].numCycles                        system.cpu.totalNumCycles
-sum  system.cpu[0-9].num_idle_cycles                  system.cpu.totalIdleCycles
-sum  system.cpu[0-9].num_int_insts                    system.cpu.int_instructions
-sum  system.cpu[0-9].num_fp_insts                     system.cpu.fp_instructions
-sum  system.cpu[0-9].Branches                         system.cpu.branch_instructions
-sum  system.cpu[0-9].num_load_insts                   system.cpu.load_instructions
-sum  system.cpu[0-9].num_store_insts                  system.cpu.store_instructions
-sum  system.cpu[0-9].committedInsts                   system.cpu.committed_instructions
-sum  system.cpu[0-9].num_int_register_reads           system.cpu.int_regfile_reads
-sum  system.cpu[0-9].num_int_register_writes          system.cpu.int_regfile_writes
-sum  system.cpu[0-9].num_fp_register_reads            system.cpu.float_regfile_reads
-sum  system.cpu[0-9].num_fp_register_writes           system.cpu.float_regfile_writes
-sum  system.cpu[0-9].num_func_calls                   system.cpu.function_calls
-sum  system.cpu[0-9].num_int_alu_accesses             system.cpu.ialu_accesses
-sum  system.cpu[0-9].num_fp_alu_accesses              system.cpu.fpu_accesses
-sum  system.cpu[0-9].itb.accesses                     system.cpu.itlb.total_accesses
-sum  system.cpu[0-9].itb.misses                       system.cpu.itlb.total_misses
-sum  system.cpu[0-9].icache.ReadReq_accesses::total   system.cpu.icache.read_accesses
-sum  system.cpu[0-9].icache.demand_misses::total      system.cpu.icache.read_misses
-sum  system.cpu[0-9].dcache.ReadReq_accesses::total   system.cpu.dcache.read_accesses
-sum  system.cpu[0-9].dcache.WriteReq_accesses::total  system.cpu.dcache.write_accesses
-sum  system.cpu[0-9].dtb.accesses                     system.cpu.dtlb.total_accesses
-sum  system.cpu[0-9].dtb.misses                       system.cpu.dtlb.total_misses
-sum  system.cpu[0-9].dcache.ReadReq_misses::total     system.cpu.dcache.read_misses
-sum  system.cpu[0-9].dcache.WriteReq_misses::total    system.cpu.dcache.write_misses
-#sum  system.mem_ctrls[0-9][0-9].readReqs              system.mem_ctrls.memory_reads
-#sum  system.mem_ctrls[0-9][0-9].writeReqs             system.mem_ctrls.memory_writes
-sum  system.mem_ctrls[0-9].readReqs              system.mem_ctrls.memory_reads # For new pim setup with only 8 mem ctrllers not associated with pim cores
-sum  system.mem_ctrls[0-9].writeReqs             system.mem_ctrls.memory_writes
+sum_float  sim_seconds                      total_sim_seconds 
+sum  numCycles                        system.cpu.totalNumCycles          system.pim.totalNumCycles
+sum  num_idle_cycles                  system.cpu.totalIdleCycles         system.pim.totalIdleCycles
+sum  num_int_insts                    system.cpu.int_instructions        system.pim.int_instructions
+sum  num_fp_insts                     system.cpu.fp_instructions         system.pim.fp_instructions
+sum  Branches                         system.cpu.branch_instructions     system.pim.branch_instructions
+sum  num_load_insts                   system.cpu.load_instructions       system.pim.load_instructions
+sum  num_store_insts                  system.cpu.store_instructions      system.pim.store_instructions
+sum  committedInsts                   system.cpu.committed_instructions  system.pim.committed_instructio
+sum  num_int_register_reads           system.cpu.int_regfile_reads       system.pim.int_regfile_reads
+sum  num_int_register_writes          system.cpu.int_regfile_writes      system.pim.int_regfile_writes
+sum  num_fp_register_reads            system.cpu.float_regfile_reads     system.pim.float_regfile_reads
+sum  num_fp_register_writes           system.cpu.float_regfile_writes    system.pim.float_regfile_writes
+sum  num_func_calls                   system.cpu.function_calls          system.pim.function_calls
+sum  num_int_alu_accesses             system.cpu.ialu_accesses           system.pim.ialu_accesses
+sum  num_fp_alu_accesses              system.cpu.fpu_accesses            system.pim.fpu_accesses
+sum  itb.accesses                     system.cpu.itlb.total_accesses     system.pim.itlb.total_accesses
+sum  itb.misses                       system.cpu.itlb.total_misses       system.pim.itlb.total_misses
+sum  icache.ReadReq_accesses::total   system.cpu.icache.read_accesses    system.pim.icache.read_accesses
+sum  icache.demand_misses::total      system.cpu.icache.read_misses      system.pim.icache.read_misses
+sum  dcache.ReadReq_accesses::total   system.cpu.dcache.read_accesses    system.pim.dcache.read_accesses
+sum  dcache.WriteReq_accesses::total  system.cpu.dcache.write_accesses   system.pim.dcache.write_accesse
+sum  dtb.accesses                     system.cpu.dtlb.total_accesses     system.pim.dtlb.total_accesses
+sum  dtb.misses                       system.cpu.dtlb.total_misses       system.pim.dtlb.total_misses
+sum  dcache.ReadReq_misses::total     system.cpu.dcache.read_misses      system.pim.dcache.read_misses
+sum  dcache.WriteReq_misses::total    system.cpu.dcache.write_misses     system.pim.dcache.write_misses
+sum  system.mem_ctrls[0-9].readReqs system.mem_ctrls.memory_reads 
+sum  system.mem_ctrls[0-9].writeReqs system.mem_ctrls.memory_writes
 sum_interconnect_accesses
 peak bw_total::total  system.mem_ctrls.peak_bandwidth
 
--- master_script.bash
+++ master_script.bash
@@ -8,27 +8,35 @@ fi
 
 xmltemplate=$1
 cacticonfig=$2
-resultfile=$3
 
-export scenarionum=12  #Unique to scenario
-echo "Getting stats and aggregating, cleaning into cut_stats.txt"
-./get_stats_and_config.bash $scenarionum
+config_path="linkedlist_experiment/pim-config.json"
+original_stats="linkedlist_experiment/pim-stats.txt"
+scenarionum=12 
 
-echo "Running gem5tomcpat to make XML for mcpat"
-python gem5tomcpat/GEM5ToMcPAT.py cut_stats.txt config.json $xmltemplate
+cp $config_path ./config.json
 
-echo "Running McPAT"
-./mcpat/mcpat -infile mcpat-out.xml -print_level 5 > mcpat_power.txt
+for num_threads in 2 4 6 8 # For 2, 4, 6, 8 threads
+do
+    grep "timestamp$((num_threads-1))\|timestamp$num_threads" $original_stats > cut_stats.txt
 
-echo "Running CACTI"
-cd cacti
-./cacti -infile $cacticonfig > ../cacti_power.txt
-cd ..
+	echo "Getting stats and aggregating, cleaning into cut_stats.txt"
+	./get_stats_and_config.bash "$((num_threads-1))" 
 
-echo "Calculating final results"
-cat mcpat_power.txt >> $resultfile
-cat cacti_power.txt >> $resultfile
-python record_results.py mcpat_power.txt cacti_power.txt >> $resultfile
+	echo "Running gem5tomcpat to make XML for mcpat"
+	python gem5tomcpat/GEM5ToMcPAT.py cut_stats.txt config.json $xmltemplate
 
+	echo "Running McPAT"
+	./mcpat/mcpat -infile mcpat-out.xml -print_level 5 > mcpat_power.txt
+
+	echo "Running CACTI"
+	cd cacti
+	./cacti -infile $cacticonfig > ../cacti_power.txt
+	cd ..
+	
+    echo "Calculating final results"
+	cat mcpat_power.txt > $num_threads-testresults.txt 
+	cat cacti_power.txt >> $num_threads-testresults.txt
+	python record_results.py mcpat_power.txt cacti_power.txt >> $num_threads-testresults.txt
+done
 
 
