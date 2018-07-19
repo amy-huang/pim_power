4684dd9afc005607ef4e5ac21693badbef0c80bf
amy-huang <amy.jiamei.huang@gmail.com>
2018-07-19 15:23:24 -0400

    generate graph put into linkedlist specific run script

--- linkedlist_experiment/get_energy.bash
+++ linkedlist_experiment/get_energy.bash
@@ -15,6 +15,6 @@ mv *-hostLL* linkedlist_experiment
 
 # pim
 ./master_script.bash tentative_arm15.xml linkedlist_experiment/pim-config.json linkedlist_experiment/pim-stats.txt cacti-out.txt pim linkedlist_experiment
-
 mv *-pim* linkedlist_experiment
 
+python generate_graph.py Linked-List-Total-Energy-Consumption Energy-in-Joules Host-Flat-Combine linkedlist_experiment/linkedlist_experiment-hostFC.tsv Host-Lazy-Lock linkedlist_experiment/linkedlist_experiment-hostLL.tsv PIM linkedlist_experiment/linkedlist_experiment-pim.tsv
